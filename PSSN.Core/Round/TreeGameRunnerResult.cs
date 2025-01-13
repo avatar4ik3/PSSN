fc32345ecee5e2b734d28999ae4b443ebcbd8ee6
Denis runduk <crossfirelet@gmail.com>
2025-01-13 23:37:13 +0600

    feat: using of parrallel game runner

--- PSSN.Core/Round/TreeGameRunnerResult.cs
+++ PSSN.Core/Round/TreeGameRunnerResult.cs
@@ -7,14 +7,14 @@ namespace PSSN.Core.Round;
 
 public class TreeGameRunnerResult
 {
-    public Dictionary<IStrategy, Dictionary<IStrategy, Dictionary<int, double>>> map = new(new StrategyComparer());
+    public ConcurrentDictionary<IStrategy, ConcurrentDictionary<IStrategy, Dictionary<int, double>>> map = new(new StrategyComparer());
 
     public Dictionary<int, double> this[IStrategy s1, IStrategy s2]
     {
         get => map[s1][s2];
         set
         {
-            if (map.ContainsKey(s1) is false) map[s1] = new Dictionary<IStrategy, Dictionary<int, double>>();
+            if (map.ContainsKey(s1) is false) map[s1] = new ConcurrentDictionary<IStrategy, Dictionary<int, double>>();
             map[s1][s2] = value;
         }
     }
@@ -24,13 +24,13 @@ public class TreeGameRunnerResult
         get => map[s1][s2][round];
         set
         {
-            if (map.ContainsKey(s1) is false) map[s1] = new Dictionary<IStrategy, Dictionary<int, double>>();
+            if (map.ContainsKey(s1) is false) map[s1] = new ConcurrentDictionary<IStrategy, Dictionary<int, double>>();
             if (map[s1].ContainsKey(s2) is false) map[s1][s2] = new Dictionary<int, double>();
             map[s1][s2][round] = value;
         }
     }
 
-    public Dictionary<IStrategy, Dictionary<int, double>> this[IStrategy s] => map[s];
+    public ConcurrentDictionary<IStrategy, Dictionary<int, double>> this[IStrategy s] => map[s];
 }
 
 public class StrategyComparer : IEqualityComparer<IStrategy>
